158cc3c1825d8b5a2b16e08f2b6d8384b0da77b0
artt <me@artt.dev>
2023-03-09 22:02:28 +0700

    added vol

--- src/components/Fx/index.tsx
+++ src/components/Fx/index.tsx
@@ -10,6 +10,10 @@ const serverAddress = process.env.NODE_ENV === "development"
 
 const curYear = new Date().getFullYear()
 
+function calculateAverage(data: number[]) {
+  return data.reduce((a, b) => a + b, 0) / data.length
+}
+
 const tickersDef: {[x: string]: any} = {
   GBPUSD: {
     label: "GBP",
@@ -23,7 +27,7 @@ const tickersDef: {[x: string]: any} = {
   },
   USDJPY: {
     label: "JPY",
-    group: "AE",
+    group: "Region",
     invert: false,
   },
   DXY: {
@@ -33,64 +37,65 @@ const tickersDef: {[x: string]: any} = {
   },
   USDTHB: {
     label: "THB",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDKRW: {
     label: "KRW",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDPHP: {
     label: "PHP",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDCNY: {
     label: "CNY",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDINR: {
     label: "INR",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDSGD: {
     label: "SGD",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDIDR: {
     label: "IDR",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDMYR: {
     label: "MYR",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDTWD: {
     label: "TWD",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
   USDVND: {
     label: "VND",
-    group: "EM",
+    group: "Region",
     invert: false,
   },
 }
 
 const graphTickers = ["USDTHB"]
 
-function getIndexOfTimestamp(ticks: number[], timestamp: number) {
+function getIndexOfTimestamp(ticks: number[], timestamp: number, goBackIfNotFoundExactly: boolean) {
   const tmp = ticks.findIndex(t => t > timestamp)
-  if (tmp === -1) {
+  if (tmp === -1)
     return ticks.length - 1
-  }
-  return tmp - 1
+  if (ticks[tmp - 1] === timestamp)
+    return tmp - 1
+  return goBackIfNotFoundExactly ? tmp - 1 : tmp
 }
 
 function ticksPercentFormatter(this: {value:number}): string {
@@ -107,6 +112,7 @@ type ProcessedData = {
     name: string,
     data: number[],
     yearlyReturns: number[]
+    yearlyVolatility: number[]
   }[]
 }
 
@@ -149,12 +155,14 @@ export default function Fx() {
         const numTicks = Math.min(...res.map(r => r.ticks.length))
         const ticks = res[0].ticks.slice(0, numTicks)
 
-        // calculate EOY dates
-        let eoyTimestamps: number[] = []
-        for (let i = -3; i <= 0; i ++) {
-          eoyTimestamps.push(getIndexOfTimestamp(ticks, Date.UTC(curYear + i, 11, 31)))
-        }
 
+        // calculate EOY dates
+        // let eoyTimestamps: number[] = []
+        // for (let i = -3; i <= 0; i ++) {
+        //   eoyTimestamps.push(getIndexOfTimestamp(ticks, Date.UTC(curYear + i, 11, 31)))
+        // }
+
+        const firstOfYearIndices = [-2, -1, 0].map(i => getIndexOfTimestamp(ticks, Date.UTC(curYear + i, 0, 1), false))
         // add in returns and volatility into the data
         const tmp = res.map(responseChunk => {
           return(responseChunk.data.map((series: {data: number[], name: string}) => {
@@ -162,23 +170,28 @@ export default function Fx() {
 
             // yearly returns
             const p = series.name === "DXY" ? 1 : -1
-            // const yearlyReturns = [0, 1, 2].map(i => (series.data[eoyTimestamps[i + 1]] / series.data[eoyTimestamps[i]])**p - 1)
-            let yearlyReturns = [NaN, NaN, NaN]
-            for (let i = 0; i < 3; i ++) {
-              yearlyReturns[i] = ((series.data[eoyTimestamps[i + 1]] / series.data[eoyTimestamps[i]])**p - 1)
+            const yearlyReturns = [0, 1, 2].map(i => (series.data[(firstOfYearIndices[i + 1] - 1) || (series.data.length - 1)] / series.data[firstOfYearIndices[i] - 1])**p - 1)
+
+            // calculate EWMA volatility
+            const weight = 0.06
+            const beginIndex = getIndexOfTimestamp(ticks, Date.UTC(2021, 0, 4), true)
+            // TODO: need to get first volatility for each series
+            const beginVol = Math.sqrt(0.0001217)
+            let vol = Array(ticks.length).fill(NaN)
+            vol[beginIndex] = beginVol
+            for (let i = beginIndex + 1; i < ticks.length; i ++) {
+              const tmpReturn = Math.log(series.data[i - 1] / series.data[i])
+              vol[i] = Math.sqrt((1 - weight) * vol[i - 1]**2 + weight * (isFinite(tmpReturn) ? tmpReturn : 0)**2)
             }
-
-            // calculate volatility
-            // const dailyReturnsSquared = series.data.map((x, i, a) => Math.log(a[i - 1] / x)**2)
-            // const 
-
-
-            // if (series.name === "USDTHB")
-            //   console.log(series.data, dailyReturns)
+            const yearlyVolatility = [0, 1, 2].map(i => calculateAverage(vol.slice(firstOfYearIndices[i], firstOfYearIndices[i + 1]).filter(x => !Number.isNaN(x))))
+            if (series.name === "DXY") {
+              console.log(series.data)
+              console.log(vol)
+            }
 
             return({
               ...series,
-              // dailyReturns: dailyReturns,
+              yearlyVolatility: yearlyVolatility.map(v => v * Math.sqrt(252)).reverse(), // annualized volatility
               yearlyReturns: yearlyReturns.reverse(), // reverse so that the most recent year is first
             })
 
@@ -195,9 +208,10 @@ export default function Fx() {
 
   interface ComparisonProps {
     yearOffset: number,
+    whatToCompare: "yearlyReturns" | "yearlyVolatility",
   }
 
-  function Comparison({ yearOffset }: ComparisonProps) {
+  function Comparison({ yearOffset, whatToCompare }: ComparisonProps) {
     return(
       <HighchartsReact
         highcharts={Highcharts}
@@ -205,11 +219,11 @@ export default function Fx() {
           chart: {
             type: 'bar',
           },
-          series: ["AE", "EM"].map(group => ({
+          series: ["AE", "Region"].map(group => ({
             name: group,
             data: getSeriesByGroup(group)?.map(series => ({
               name: tickersDef[series.name].label,
-              y: series.yearlyReturns[yearOffset],
+              y: series[whatToCompare][yearOffset],
               color: series.name === "USDTHB" && "coral",
             })),
             dataSorting: {
@@ -224,18 +238,18 @@ export default function Fx() {
           xAxis: [
             {
               type: 'category',
-              height: '28%',
+              height: '21%',
             },
             {
               type: 'category',
-              height: '70%',
-              top: '30%',
+              height: '77%',
+              top: '23%',
               offset: 0,
             }
           ],
           yAxis: {
             title: {
-              text: `${curYear - yearOffset} Returns` + (yearOffset === 0 ? " (YTD)" : ""),
+              text: `${curYear - yearOffset} ${whatToCompare.replace('yearly', '')}` + (yearOffset === 0 ? " (YTD)" : ""),
             },
             labels: {
               formatter: ticksPercentFormatter,
@@ -246,7 +260,7 @@ export default function Fx() {
           },
           plotOptions: {
             series: {
-              enableMouseTracking: false,
+              enablRegionouseTracking: false,
             },
           },
           legend: {
@@ -261,37 +275,44 @@ export default function Fx() {
   }
 
   return(
-    <div>
-      <HighchartsReact
-        highcharts={Highcharts}
-        constructorType={'stockChart'}
-        options={{
-          series: processedData?.series
-            .filter(series => graphTickers.includes(series.name))
-            .map(series => ({
-              name: series.name,
-              data: series.data.map((p, i) => [processedData.ticks[i], p])
-            })),
-          scrollbar: {
-            enabled: false
-          },
-          plotOptions: {
-            series: {
-              // compare: 'percent',
-              // showInNavigator: true,
-            }
-          },
-          credits: {
-            enabled: false,
-          },
-        }}
-      />
-      <div className={styles.container}>
-        <Comparison yearOffset={2}/>
-        <Comparison yearOffset={1}/>
-        <Comparison yearOffset={0}/>
+    <>
+      <div className={styles.timeSeriesContainer}>
+        <HighchartsReact
+          highcharts={Highcharts}
+          constructorType={'stockChart'}
+          options={{
+            series: processedData?.series
+              .filter(series => graphTickers.includes(series.name))
+              .map(series => ({
+                name: series.name,
+                data: series.data.map((p, i) => [processedData.ticks[i], p])
+              })),
+            scrollbar: {
+              enabled: false
+            },
+            plotOptions: {
+              series: {
+                // compare: 'percent',
+                // showInNavigator: true,
+              }
+            },
+            credits: {
+              enabled: false,
+            },
+          }}
+        />
       </div>
-    </div>
+      <div className={styles.comparisonContainer}>
+        <div className={styles.flexContainer}>
+          <Comparison yearOffset={2} whatToCompare="yearlyReturns" />
+          <Comparison yearOffset={1} whatToCompare="yearlyReturns" />
+          <Comparison yearOffset={0} whatToCompare="yearlyReturns" />
+          <Comparison yearOffset={2} whatToCompare="yearlyVolatility" />
+          <Comparison yearOffset={1} whatToCompare="yearlyVolatility" />
+          <Comparison yearOffset={0} whatToCompare="yearlyVolatility" />
+        </div>
+      </div>
+    </>
   )
 
 }
