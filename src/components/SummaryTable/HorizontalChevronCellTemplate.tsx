552f355b4e69321179151ab7b70f9ea5a33546b5
artt <me@artt.dev>
2023-09-10 21:15:38 +0700

    x

--- src/components/SummaryTable/HorizontalChevronCellTemplate.tsx
+++ src/components/SummaryTable/HorizontalChevronCellTemplate.tsx
@@ -2,8 +2,8 @@
 
 import * as React from 'react';
 import {
-  CellTemplate, Cell, Compatible, Uncertain, UncertainCompatible, Id, isNavigationKey,
-  isAlphaNumericKey, getCellProperty
+  CellTemplate, Cell, Compatible, Uncertain, UncertainCompatible, Id, getCellProperty
+  // isNavigationKey, isAlphaNumericKey, 
 } from "@silevis/reactgrid";
 
 export interface HorizontalChevronCell extends Cell {
@@ -41,27 +41,27 @@ export class HorizontalChevronCellTemplate implements CellTemplate<HorizontalChe
 
   isFocusable = () => false;
 
-  handleKeyDown(cell: Compatible<HorizontalChevronCell>, keyCode: number, ctrl: boolean, shift: boolean, alt: boolean): { cell: Compatible<HorizontalChevronCell>, enableEditMode: boolean } {
-    // let enableEditMode = keyCode === keyCodes.POINTER || keyCode === keyCodes.ENTER;
-    const cellCopy = { ...cell };
-    // const char = getCharFromKeyCode(keyCode, shift);
-    // if (keyCode === keyCodes.SPACE && cellCopy.isExpanded !== undefined && !shift) {
-    //     cellCopy.isExpanded = !cellCopy.isExpanded;
-    // } else if (!ctrl && !alt && isAlphaNumericKey(keyCode) && !(shift && keyCode === keyCodes.SPACE)) {
-    //     cellCopy.text = !shift ? char.toLowerCase() : char;
-    //     enableEditMode = true;
-    // }
-    return { cell: cellCopy, enableEditMode: false }; // FORCED DISABLED EDIT MODE
-  }
+  // handleKeyDown(cell: Compatible<HorizontalChevronCell>, keyCode: number, ctrl: boolean, shift: boolean, alt: boolean): { cell: Compatible<HorizontalChevronCell>, enableEditMode: boolean } {
+  //   // let enableEditMode = keyCode === keyCodes.POINTER || keyCode === keyCodes.ENTER;
+  //   const cellCopy = { ...cell };
+  //   // const char = getCharFromKeyCode(keyCode, shift);
+  //   // if (keyCode === keyCodes.SPACE && cellCopy.isExpanded !== undefined && !shift) {
+  //   //     cellCopy.isExpanded = !cellCopy.isExpanded;
+  //   // } else if (!ctrl && !alt && isAlphaNumericKey(keyCode) && !(shift && keyCode === keyCodes.SPACE)) {
+  //   //     cellCopy.text = !shift ? char.toLowerCase() : char;
+  //   //     enableEditMode = true;
+  //   // }
+  //   return { cell: cellCopy, enableEditMode: false }; // FORCED DISABLED EDIT MODE
+  // }
 
-  getClassName(cell: Compatible<HorizontalChevronCell>, isInEditMode: boolean) {
+  getClassName(cell: Compatible<HorizontalChevronCell>, _isInEditMode: boolean) {
     const isExpanded = cell.hasChildren ? cell.isExpanded ? 'expanded' : 'collapsed' : '';
     const className = cell.className || '';
     return `${isExpanded} ${className}`;
   }
 
   // render(cell: Compatible<HorizontalChevronCell>, isInEditMode: boolean, onCellChanged: (cell: Compatible<HorizontalChevronCell>, commit: boolean) => void): React.ReactNode {
-  render(cell: Compatible<HorizontalChevronCell>, isInEditMode: boolean, onCellChanged: (cell: Compatible<HorizontalChevronCell>, commit: boolean) => void): React.ReactNode {
+  render(cell: Compatible<HorizontalChevronCell>, _isInEditMode: boolean, onCellChanged: (cell: Compatible<HorizontalChevronCell>, commit: boolean) => void): React.ReactNode {
     return (
       <>
         {cell.hasChildren ?
